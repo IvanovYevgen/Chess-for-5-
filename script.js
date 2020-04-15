11e6ffdea264380641f03b6843280258bcb4191e
--global <johndoe@example.com>
2020-04-15 15:47:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,137 @@
+
+
+
+// again and again
+const useCircle = false;
+
+// create board
+let board = document.createElement('div')
+board.classList.add('board');
+document.body.appendChild(board);
+
+// create cells
+for (let i = 0; i < 25; i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    board.appendChild(cell);
+}
+
+let cells = document.querySelectorAll('.cell');
+let x = 1, y = 5;
+cells.forEach(function (cell) {
+    if (x > 5) {
+        x = 1;
+        y--;
+    }
+    cell.setAttribute('data-pos-x', x++);
+    cell.setAttribute('data-pos-y', y);
+
+    if ((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0)) {
+        cell.style.backgroundColor = '#fad390';
+    } else {
+        cell.style.backgroundColor = '#3E2723';
+    }
+});
+
+// set horse to cell
+let index = Math.round(Math.random() * 63);
+cells[index].classList.add('current');
+cells[index].classList.add('set');
+
+// set number for cell
+let step = 1;
+cells[index].innerHTML = step;
+
+// gets x and y for first cell
+let currentX = cells[index].getAttribute('data-pos-x');
+let currentY = cells[index].getAttribute('data-pos-y');
+
+// init horse move matrix
+const horseMoves = [
+    [-2, 1], [-2, -1], // left
+    [-1, 2], [1, 2],   // top
+    [2, -1], [2, 1],   // right
+    [1, -2], [-1, -2]  // down
+];
+
+let interval = setInterval(() => {
+    move(currentX, currentY)
+}, 50);
+
+
+
+// move horse
+function move(x, y) {
+    debug("----------------------------");
+    debug("Step: ", step);
+    let possibleMoves = getPosibleMoveCells(horseMoves, x, y);
+
+    if (possibleMoves.length == 0) {
+        alert("The end!");
+    }
+    // debug
+    debug("Possible moves:", possibleMoves);
+
+    let possibleMoveCount = possibleMoves.map(element => {
+        const _x = element.getAttribute('data-pos-x');
+        const _y = element.getAttribute('data-pos-y');
+        return getPosibleMoveCells(horseMoves, _x, _y).length;
+    });
+
+    // debug
+    debug("Possible moves in layer 2:", possibleMoveCount);
+
+    // get index for min possible move count
+    let minIndex = possibleMoveCount.indexOf(getMinMoveCount(possibleMoveCount));
+    // debuf
+    debug("Min move count index:", minIndex);
+
+    // clear select cell
+    document.querySelector('.current').classList.remove('current');
+    // move to next cell
+    let nextCell = possibleMoves[minIndex];
+    // debug
+    debug("Next cell:", nextCell);
+
+    nextCell.classList.add('current');
+    nextCell.classList.add('set');
+    nextCell.innerHTML = ++step;
+
+    // set new value for x and y
+    currentX = nextCell.getAttribute('data-pos-x');
+    currentY = nextCell.getAttribute('data-pos-y');
+
+    if (step == 64) {
+        clearInterval(interval);
+        setTimeout(() => {
+            alert("Good job!");
+            if (useCircle) document.location.reload(true);
+        }, 200);
+    }
+}
+
+// get can move cells
+function getPosibleMoveCells(moveTemplate, x, y) {
+    let possibleCells = new Array;
+    let count = 0;
+    moveTemplate.forEach(function (move) {
+        let _x = +x + move[0];
+        let _y = +y + move[1];
+        let cell = document.querySelector(`.cell[data-pos-x="${_x}"][data-pos-y="${_y}"]`);
+        if (cell && !cell.classList.contains('set')) {
+            possibleCells[count++] = cell;
+        }
+    });
+    return possibleCells;
+}
+
+// gets minial value in array
+function getMinMoveCount(arr) {
+    return Math.min.apply(null, arr);
+}
+
+// log function
+function debug(message, value = null) {
+    if (value != null) console.log(message, value);
+    else console.log(message);
+}
